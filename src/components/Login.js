dd2be951536821094e0322d1a73645662b97cdbe
axeljk <axeljkern@yahoo.com>
2022-08-31 12:44:39 -0400

    Updates login page to utilize MUI components in place of plain HTML tags and adds some CSS styling for the layout of the page.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,36 @@
-import React, { useEffect } from 'react'
 import AuthForm from './AuthForm'
 import { useNavigate } from 'react-router-dom'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
 
 export default function Login(props) {
-    const navigate = useNavigate();
-    useEffect(()=>{
-        if(props.userId){
-            navigate(`/users/${props.userId}`)
-        }
-    }, [props.userId])
-    return (
-        <div>
-            <AuthForm type='Login' handleSubmit={props.handleLogin}/>
-            <AuthForm type='Signup' handleSubmit={props.handleSignup}/>
-        </div>
-    )
+	return (
+		<div className="heroContainer" sx={{}}>
+			<Card variant="outlined" sx={{width:350, boxShadow: 8}}>
+				<CardContent>
+					<Typography align="center" variant="h4" sx={{mb: 1}}>Log in</Typography>
+					<form onSubmit={props.handleSignup} autoComplete="on">
+						<TextField required id="outlined-required" label="Email" fullWidth margin="dense" size="small" name="email" />
+						<TextField required id="outlined-required" type="password" fullWidth label="Password" margin="dense" size="small" sx={{mb:2}} name="password" />
+						<div className="cardAction">
+							<Button type="submit" size="small" sx={{fontWeight: "bold"}}>Submit</Button>
+						</div>
+					</form>
+					<Typography variant="body2" sx={{color: "error.dark", fontWeight: "bold"}}></Typography>
+				</CardContent>
+			</Card>
+			<Card variant="outlined" sx={{width:350, boxShadow: 8}}>
+				<CardActions className="signupContainer">
+					<Typography>Don't have an account?&nbsp;</Typography>
+					<Link href="/signup" key="signup" underline="hover" sx={{fontWeight: "bold"}}>Sign up</Link>
+				</CardActions>
+			</Card>
+		</div>
+	);
 }
