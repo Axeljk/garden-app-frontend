5840fab68b73742ff7fd3211fba9d36e1cd9a151
axeljk <axeljkern@yahoo.com>
2022-08-31 18:51:48 -0400

    Login/signup routes work now for the most part. Still can't get error messages to return from the handleSubmit functions and have no idea how to redirect the page but otherwise... okay?

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,3 @@
-import AuthForm from './AuthForm'
-import { useNavigate } from 'react-router-dom'
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,26 +6,38 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
+import "./login.css";
 
 export default function Signup(props) {
+	const [error, setError] = React.useState("");
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const response = props.handleSignup(event.target[0].value, event.target[2].value, event.target[4].value);
+		console.log(response);
+
+		if (response?.message)
+			setError(response.message);
+	}
+
 	return (
 		<div className="heroContainer" sx={{}}>
-			<Card variant="outlined" sx={{width:350, boxShadow: 8}}>
+			<Card variant="outlined" sx={{maxWidth:350, boxShadow: 8}}>
 				<CardContent>
 					<Typography align="center" variant="h4" sx={{mb: 1}}>Sign up</Typography>
 					<Typography align="center" variant="subtitle1" sx={{mb: 1}}>Sign up to begin planning your garden.</Typography>
-					<form onSubmit={props.handleSignup} autoComplete="on">
-						<TextField required id="outlined-required" label="Username" fullWidth margin="dense" size="small" name="username" />
-						<TextField required id="outlined-required" label="Email" fullWidth margin="dense" size="small" name="email" />
-						<TextField required id="outlined-required" type="password" fullWidth label="Password" margin="dense" size="small" sx={{mb:2}} name="password" />
+					<form onSubmit={handleSubmit} autoComplete="on">
+						<TextField required className="outlined-required" label="Username" fullWidth margin="dense" size="small" name="username" />
+						<TextField required className="outlined-required" label="Email" fullWidth margin="dense" size="small" name="email" />
+						<TextField required className="outlined-required" type="password" fullWidth label="Password" margin="dense" size="small" sx={{mb:2}} name="password" />
 						<div className="cardAction">
 							<Button type="submit" size="small" sx={{fontWeight: "bold"}}>Submit</Button>
 						</div>
 					</form>
-					<Typography variant="body2" sx={{color: "error.dark", fontWeight: "bold"}}></Typography>
+					<Typography variant="body2" sx={{color: "error.dark", fontWeight: "bold"}}>{error}</Typography>
 				</CardContent>
 			</Card>
-			<Card variant="outlined" sx={{width:350, boxShadow: 8}}>
+			<Card variant="outlined" sx={{maxWidth:350, width: "100%", boxShadow: 8}}>
 				<CardActions className="signupContainer">
 					<Typography>Already have an account?&nbsp;</Typography>
 					<Link href="/login" key="login" underline="hover" sx={{fontWeight: "bold"}}>Log in</Link>
