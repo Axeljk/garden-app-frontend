16d2a2230c4ab4778bfe9df4ab33df34af5b35f5
axeljk <axeljkern@yahoo.com>
2022-09-01 17:25:17 -0400

    Menu to other pages only shows when logged in. Login link only displays when not already logged in.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,7 @@ import Typography from '@mui/material/Typography'
 import HomeRepairServiceIcon from '@mui/icons-material/HomeRepairService'
 import './NavBar.css'
 
-const pages = ['Login/Signup', 'About'];
+const pages = ['Login', 'About'];
 
 export default function NavBar(props) {
 
@@ -90,13 +90,14 @@ export default function NavBar(props) {
                     </Box>
                     {/* Login & About buttons */}
                     <Box sx={{ width: '50%', flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+						{props.userId == 0 &&
                         <Link to='/login' style={{textDecoration:'none'}}><Button
                                 key='login'
                                 onClick={handleCloseNavMenu}
                                 sx={{ my: 2, color: 'white', display: 'block' }}
                         >
-                        Login/Signup
-                        </Button></Link>
+                        Login
+                        </Button></Link>}
                         <Link to='/about' style={{textDecoration:'none'}}><Button
                         key='about'
                         onClick={handleCloseNavMenu}
@@ -125,6 +126,7 @@ export default function NavBar(props) {
                         Peas By Spring
                     </Typography>
                     {/* Avatar & Account dropdown */}
+					{props.userId != 0 &&
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Tool Shed">
                             {/* <Link to={`/users/$props.userId}`}> */}
@@ -157,12 +159,12 @@ export default function NavBar(props) {
                             <MenuItem component={Link} to='/settings'>Account Settings</MenuItem>
 
                             {/* {settings.map((setting) => (
-                                
+
                                     <Typography textAlign="center">{setting}</Typography>
                                 </MenuItem>
                             ))} */}
                         </Menu>
-                    </Box>
+                    </Box>}
 
                 </Toolbar>
             </Container>
