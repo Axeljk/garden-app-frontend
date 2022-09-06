b77272d8a097a07b3b00786c84c0d8726a78e56b
axeljk <axeljkern@yahoo.com>
2022-09-05 22:28:40 -0400

    Styling garden.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,79 +1,19 @@
 
-  import React, {useState, useEffect} from 'react'
-  import Grid from '@mui/material/Grid';
-  import './Layout.css'
-  import GridLines from 'react-gridlines';
-  import soil1 from '../assets/Plants/soil1.jpg'
-  import ImageList from '@mui/material/ImageList';
-  import ImageListItem from '@mui/material/ImageListItem';
-  import Stack from '@mui/material/Stack';
-  import LayoutMenu from "../components/LayoutMenu";
-  import LayoutPicker from "../components/LayoutPicker";
+import React, {useState, useEffect} from 'react'
+import Grid from '@mui/material/Grid';
+import './Layout.css'
+import GridLines from 'react-gridlines';
+import soilImg from '../assets/Plants/soil1.jpg'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Stack from '@mui/material/Stack';
+import LayoutMenu from "../components/LayoutMenu";
+import LayoutPicker from "../components/LayoutPicker";
 import { Typography } from '@mui/material';
 import API from "../utils/API";
 
-  function Layout(props) {
-
-    const itemData = [
-      {
-        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-        title: 'Breakfast',
-        maxHeight: '100px',
-        maxWidth: '100px'
-      },
-      {
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/1920px-Patates.jpg',
-        title: 'Potato',
-        maxHeight: '100px',
-        maxWidth: '100px'
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-        maxHeight: '100px',
-        maxWidth: '100px'
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-        title: 'Coffee',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-        title: 'Hats',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-        title: 'Honey',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-        title: 'Basketball',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-        title: 'Fern',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
-      },
-      {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
-      },
-    ];
-
-    const [dimensionx, setDimensionx] = useState(0);
-    const [dimensiony, setDimensiony] = useState(0);
-    const[soilImg,setSoilImg] = useState(soil1);
+function Layout(props) {
+	// States used for the page.
     const [garden, setGarden] = useState([]);
 
     const[squareBeingDragged,setSquareBeingDragged] = useState(null);
@@ -181,9 +121,14 @@ const dragStart = (e) =>{
     }
 
     const box1 = {
-      width:"100px",
-      height:"100px",
-
+		width:"100px",
+		height:"100px",
+		backgroundImage: `url(${soilImg})`,
+		padding: "10px 10px",
+		boxShadow:`0px 10px 10px rgba(0,0,0,0.1)`,
+		borderStyle: "none",
+		outline: "none",
+		borderWidth: 0
     }
     const box2 = {
       width:"100px",
@@ -204,9 +149,9 @@ const dragStart = (e) =>{
       let arr = [];
       let id=1;
       for (let i=0;i<gardenData.width;i++){
-        let temp = [];
+        let row = [];
         for (let j=0;j<gardenData.height;j++){
-            temp.push(<img style={box1} key={i+j} src={soilImg}  data-id={id} draggable={true}
+            row.push(<img style={box1} sx={{border: 0}} key={i+j} data-id={id} draggable={true}
               onDragStart={dragStart}
               onDragOver={(e)=> e.preventDefault()}
               onDragEnter={(e)=> e.preventDefault()}
@@ -215,7 +160,7 @@ const dragStart = (e) =>{
               onDragEnd={dragEnd}></img>);
               id++;
         }
-        arr.push(temp);
+        arr.push(row);
       }
 
       setGarden(arr);
@@ -240,13 +185,6 @@ const dragStart = (e) =>{
           </GridLines>
           </Grid>
           <Grid item xs={4}>
-          <div className='app'>
-          <Stack spacing={2}>
-            <span>N x N Garden Layout</span>
-          <input type="number" placeholder='Enter the Dimension' onChange={(e)=> setDimensionx(e.target.value<20 ? e.target.value : 20)} />
-          <input type="number" placeholder='Enter the Dimension' onChange={(e)=>setDimensiony(e.target.value)} />
-          </Stack>
-            </div>
           <div className='images-div'><h1>Select the Plant</h1>
 
 
