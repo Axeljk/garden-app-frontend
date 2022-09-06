872de14d765bf336401986a9ecd0f4c01872d98e
kristensantee <mikeandkris27@gmail.com>
2022-09-05 17:33:08 -0700

    started settings, not much progress

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider'
+import Box from '@mui/material/Box'
 
+const style = {
+  width: '100%',
+  maxWidth: 360,
+  bgcolor: 'background.paper',
+};
 
 export default function Settings() {
 
-
     return (
-      <div></div>
+      <Box>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Name" />
+        </ListItem>
+        <Divider />
+        <ListItem button divider>
+          <ListItemText primary="Email" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Password" />
+        </ListItem>
+        <Divider light />
+        <ListItem button>
+          <ListItemText primary="Location" />
+        </ListItem>
+        </List>
+      </Box>
     )
 }
